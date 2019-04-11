e0eea308337bc11d5a605eec68551d35ed27bbff
Uriel Espinoza <urielintemperie@gmail.com>
2019-04-10 23:05:23 -0300

    Prueba Codecov

---
+++ desapp-groupF-backend/src/main/backend/modelo/HelloWorld.java
@@ -0,0 +1,13 @@
+package backend.modelo;
+
+public class HelloWorld {
+
+    public String getMessage(boolean bigger) {
+        if (bigger) {
+            return "Hello Universe!";
+        } else {
+            return "Hello World!";
+        }
+    }
+
+}
