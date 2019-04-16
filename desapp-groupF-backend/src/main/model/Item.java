c9a72da00b3d5bf8c4339910f77a35fe032c8220
Uriel Espinoza <urielintemperie@gmail.com>
2019-04-16 20:05:17 -0300

    Item agrego constructor y corrijo error sintaxis

--- desapp-groupF-backend/src/main/model/Item.java
+++ desapp-groupF-backend/src/main/model/Item.java
@@ -1,8 +1,13 @@
 package model;
 
 public class Item {
-    private Product product
+    private Product product;
     private Integer amount;
+
+    public Item (Product product, Integer amount) {
+        this.product = product;
+        this.amount = amount;
+    }
 
     public float price() {
         return product.price * amount;
