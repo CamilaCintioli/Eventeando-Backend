d0b8e41c93e668dcd47aeddb697981829fee2bca
camilacintioli <camila.cintioli@gmail.com>
2019-06-28 16:55:01 -0300

    Agrego validaciones en LoanController

--- src/main/java/ar/edu/unq/desapp/grupoF/desappgrupoFbackend/controller/LoanController.java
+++ src/main/java/ar/edu/unq/desapp/grupoF/desappgrupoFbackend/controller/LoanController.java
@@ -5,6 +5,7 @@ import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.dto.TransferenceDTO;
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.economy.Loan;
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,14 +21,14 @@ public class LoanController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/loan/new/{email}")
-    public Loan createLoan(@PathVariable("email") String email){
+    public ResponseEntity createLoan(@PathVariable("email") String email){
 
         return loanService.giveLoan(email);
 
     }
 
     @GetMapping("loan/{email}")
-    public Loan getLoan(@PathVariable("email") String email){
+    public ResponseEntity getLoan(@PathVariable("email") String email){
 
         return  loanService.getLoan(email);
 
--- src/main/java/ar/edu/unq/desapp/grupoF/desappgrupoFbackend/service/LoanService.java
+++ src/main/java/ar/edu/unq/desapp/grupoF/desappgrupoFbackend/service/LoanService.java
@@ -2,11 +2,10 @@ package ar.edu.unq.desapp.grupoF.desappgrupoFbackend.service;
 
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.economy.Account;
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.economy.Loan;
-import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.exceptions.ThereIsNoLoanException;
-import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.model.exceptions.UserIsNotEligibleToReceiveALoanException;
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.repository.AccountRepository;
 import ar.edu.unq.desapp.grupoF.desappgrupoFbackend.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,18 +24,19 @@ public class LoanService {
         this.accountRepository = accountRepository;
     }
 
-    public Loan giveLoan(String email){
-
-        //TODO: tirar error si no encuentra el email.
+    public ResponseEntity giveLoan(String email){
 
         Account account = accountRepository.findByEmailUser(email);
 
-        if (!isEligibleForLoan(email)) throw new UserIsNotEligibleToReceiveALoanException();
+        if(account==null) return ResponseEntity.badRequest().body("The given mail does not have an account");
+
+        if (!isEligibleForLoan(email)) return ResponseEntity.badRequest().body("The user is not elegible for a loan");
+
         Loan loan = new Loan(email, account);
 
         loanRepository.save(loan);
 
-        return loan;
+        return ResponseEntity.ok(loan);
     }
 
     private boolean isEligibleForLoan(String email) {
@@ -64,14 +64,13 @@ public class LoanService {
         }
     }
 
-    public Loan getLoan(String email) {
+    public ResponseEntity getLoan(String email) {
 
         Optional<Loan> loan = Optional.ofNullable(loanRepository.findByUserEmail(email));
 
-        if(! loan.isPresent()){
-            throw new ThereIsNoLoanException("There is not loan for this user" + email);
-        }
-        return loan.get();
+        if(! loan.isPresent()) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(loan.get());
 
         
 
